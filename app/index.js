72ca90b0901db7e91ec473c3c2344e7e2379df40
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-26 14:19:46 +0300

    app: added simple component for the rendering

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from 'components/Greeting';
+
+function App() {
+	return <Greeting name='Ann' />;
+}
+
+ReactDOM.render(<App />, document.querySelector('main'));
