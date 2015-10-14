df54282fa02dad0e55aeae821dee7e15eb7539c2
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-14 18:18:58 +0300

    use common logger

--- index.node.js
+++ index.node.js
@@ -8,8 +8,8 @@ import Layout from './layout';
 const PORT = process.env.PORT || 3000;
 const app = Express();
 
+app.use(Morgan('common'));
 app.use(Express.static('./dist'));
-app.use(Morgan('dev'));
 
 app.get('/', (request, response) => response.send(Layout('Evo', styles, scripts, React.renderToString(<App />))));
 
