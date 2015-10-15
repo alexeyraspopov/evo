3562c6e757aeb662ac47ed12e079907256886f34
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-15 18:53:39 +0300

    static and port as ENV variables

--- server/index.node.js
+++ server/index.node.js
@@ -1,16 +1,21 @@
 import Express from 'express';
 import Morgan from 'Morgan';
 import React from 'react';
-import App from './app/components/App';
+import App from '../app/components/App';
 import {styles, scripts} from './params.node';
 import Layout from './layout';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || throwError('Port should be specified by environment variable');
+const STATIC = process.env.STATIC || throwError('Static folder should be specified by environment variable');
 const app = Express();
 
 app.use(Morgan('common'));
-app.use(Express.static('./dist'));
+app.use(Express.static(STATIC));
 
 app.get('/', (request, response) => response.send(Layout('Evo', styles, scripts, React.renderToString(<App />))));
 
 app.listen(PORT);
+
+function throwError(msg) {
+	throw new Error(msg);
+}
