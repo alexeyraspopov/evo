a30d1561835ac4f778dcc512ef28d7a634fc65da
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-18 16:03:18 +0300

    misc

--- app/components/App.js
+++ app/components/App.js
@@ -2,5 +2,6 @@ import React from 'react';
 import Greeting from 'components/Greeting';
 
 export default function App({path}) {
-	return <Greeting name={"Ann"+path} />;
+	// TODO: path -> params
+	return <Greeting name={`Ann ${path}`} />;
 }
