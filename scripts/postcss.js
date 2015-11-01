6552f7515f4c3b668e5345036a72d5d8cb5cb6a9
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-11-01 20:25:54 +0200

    compile styles

---
+++ scripts/postcss.js
@@ -0,0 +1,19 @@
+import postcss from 'postcss';
+import imports from 'postcss-import';
+import autoprefixer from 'autoprefixer';
+import minimist from 'minimist';
+import {createReadStream, createWriteStream} from 'fs';
+import map from 'map-stream';
+import path from 'path';
+
+const argv = minimist(process.argv.slice(2));
+const entry = path.join(process.cwd(), argv.e);
+const output = path.join(process.cwd(), argv.o);
+const processor = postcss([autoprefixer, imports]);
+
+createReadStream(entry)
+	.pipe(map((data, cb) => {
+		processor.process(data)
+			.then(result => cb(null, result.css), cb);
+	}))
+	.pipe(createWriteStream(output));
