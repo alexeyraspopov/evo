94e8cc026155f49c07daff71fa9a1e69eb3db3ec
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-11-07 14:21:29 +0200

    added action buttons

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ActionButton from 'components/ActionButton';
+
 export default function Header() {
 	return (
 		<header className="header">
+			<ActionButton type="menu" />
 			<h2 className="header-title">Users</h2>
+			<ActionButton type="search" />
+			<ActionButton type="more_vert" />
 		</header>
 	);
 }
